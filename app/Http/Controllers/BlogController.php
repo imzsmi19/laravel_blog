b6cddf7d58c6c0bd43051854734613c02eb6dbb7
imzsmi19 <imzsmi19@gmail.com>
2021-10-28 15:58:19 +0700

    Add method create, store n view create

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Blog;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    /**
+     * index
+     * 
+     * @return void
+     */
+    public function index(){
+        $blogs = Blog::latest()->paginate(10);
+        return view('blog.index', compact('blogs'));
+    }
+
+    /**
+     * create
+     * 
+     * @return void
+     */
+    public function create(){
+        return view('blog.create');
+    }
+
+    /**
+     * store
+     * 
+     * @param mixed $request
+     * @return void
+     */
+    public function store(Request $request){
+        $this->validate($request, [
+            'image'=> 'required|image|mimes:png,jpg,jpeg',
+            'title'=> 'required',
+            'content'=> 'required'
+        ]);
+
+        // upload image
+        $image = $request->file('image');
+        $image->storeAs('public/blogs', $image->hashName());
+
+        $blog = Blog::create([
+            'image'=> $image->hashName(),
+            'title'=> $request->title,
+            'content'=> $request->content
+        ]);
+
+        if($blog){
+            // redirect dengan pesan sukses
+            return redirect()->route('blog.index')->with(['success'=>'Data berhasil disimpan!']);
+        }else{
+            // redirect dengan pesan error
+            return redirect()->route('blog.index')->with(['error'=>'Data gagal disimpan!']);
+        }
+    }
+}
